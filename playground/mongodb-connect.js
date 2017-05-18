b8cbeb81eba237715af9e46932e928c7924bf56a
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-18 09:01:18 -0700

    the objectid

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -18,17 +22,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // });
   
   // Insert new doc into Users (name, age, location)
-  db.collection('Users').insertOne({
-    name: 'Hugo',
-    age: 33,
-    location: 'Atlanta'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err);
-    }
-    
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Hugo',
+  //   age: 33,
+  //   location: 'Atlanta'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
   
   db.close();
 });
