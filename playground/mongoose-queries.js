fc85ec94fb93688574396411a183730c711c0428
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-23 08:22:06 -0700

    mongoose queries and id validation

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,60 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '592395e69e553bee21ccb7c7';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+
+User.findById('59225ae0da819307f658f524').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+  
+  console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
