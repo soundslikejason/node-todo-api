5a7b748fd40976396670e12b03ad508828bf54a6
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-24 16:29:42 -0700

    deploy api to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://puff:daddy@ds153400.mlab.com:53400/node-todo-api'
+};
+mongoose.connect(db.localhost || db.mlab);
 
 module.exports = {mongoose};
