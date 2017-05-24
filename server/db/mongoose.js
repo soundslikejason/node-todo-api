3b63cc9f42e70fae4cd13774edcc6c1a7780df0a
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-24 16:36:43 -0700

    mongoose connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://puff:daddy@ds153400.mlab.com:53400/node-todo-api'
 };
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
 module.exports = {mongoose};
