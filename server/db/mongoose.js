24534b22088d7cbc84029f9dfeb66613069934d8
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-24 21:33:00 -0700

    mongoose connect refactor

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: process.env.MONGODB_URI
-};
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
