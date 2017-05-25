2ce90435d1fa33e128ccc627729ff4e468535206
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-24 17:21:14 -0700

    heroku config var

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://puff:daddy@ds153400.mlab.com:53400/node-todo-api'
+  mlab: MONGODB_URI
 };
 mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
